d93966094574fb7e931f88bc3b70066d40474172
al721jae <al721jae@htwg-konstanz.de>
2020-03-21 13:57:52 +0100

    maybe it works

--- app/src/main/java/com/example/carryonde/MainController.java
+++ app/src/main/java/com/example/carryonde/MainController.java
@@ -1,4 +1,27 @@
 package com.example.carryonde;
 
 public class MainController {
+    private boolean isHelper;
+    private MainController instance;
+    private MainController(){
+
+    }
+
+    public MainController getMainController(){
+        if(this.instance == null) {
+            this.instance = new MainController();
+        }
+        return this.instance;
+    }
+
+    public boolean userIsHelper(){
+        return this.isHelper;
+    }
+    public void makeUserHelper(){
+        this.isHelper = true;
+    }
+
+    public void makeUserOrga(){
+        this.isHelper = false;
+    }
 }
--- app/src/main/java/com/example/carryonde/ui/WelcomeActivity.java
+++ app/src/main/java/com/example/carryonde/ui/WelcomeActivity.java
@@ -1,6 +1,37 @@
 package com.example.carryonde.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WelcomeActivity extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.carryonde.R;
+
+public class WelcomeActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button buttonHelper = findViewById(R.id.buttonHelper);
+        Button buttonOrganisation = findViewById(R.id.buttonOrganisation);
+        buttonHelper.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonHelper:
+                break;
+                //MainController.ins
+        }
+
+
+
+    }
 }
